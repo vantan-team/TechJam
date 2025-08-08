d87c10d8fdfc9998d17acd8b29c9988e40e7363a
Kaito0523 <kaito.tanabe0523@gmail.com>
2025-08-08 15:44:32 +0900

    user画面api

--- back/routes/api.php
+++ back/routes/api.php
@@ -9,6 +9,9 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RestaurantController;
 use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\VisitedShopController;
+use App\Http\Controllers\GuideBookController;
+use App\Http\Controllers\GuideContentController;
+use App\Http\Controllers\UserController;
 
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
@@ -19,6 +22,9 @@ Route::get('/home', [HomeController::class, 'index']);
 // Restaurant endpoints
 Route::get('/restaurants/{hotpepperId}/detail', [RestaurantController::class, 'getShopDetail']);
 Route::post('/restaurants/search', [RestaurantController::class, 'search']);
+
+// User public profile endpoint
+Route::post('/user/{id}', [UserController::class, 'show'])->whereNumber('id');
 
 
 Route::middleware('auth:sanctum')->group(function () {
@@ -35,18 +41,31 @@ Route::middleware('auth:sanctum')->group(function () {
     // User endpoints (authenticated)
     Route::post('/user/follow', [FollowController::class, 'follow']);
     Route::post('/user/{id}/followed', [FollowController::class, 'getFollowed']);
+    Route::post('/user/{id}/guide_books', [UserController::class, 'guidebooks'])->whereNumber('id');
     Route::post('/user/{id}/visited_history', [VisitedShopController::class, 'getUserHistory']);
 
     // Restaurant endpoints (authenticated)
     Route::post('/restaurants/add-to-guide', [RestaurantController::class, 'addToGuide']);
-    
+
     // Notification endpoints (authenticated)
     Route::get('/notifications', [NotificationController::class, 'index']);
     Route::get('/notifications/unread-count', [NotificationController::class, 'unreadCount']);
     Route::post('/notifications/mark-as-read', [NotificationController::class, 'markAsRead']);
-    
+
     // Visited Shop History endpoints (authenticated)
     Route::post('/history/add', [VisitedShopController::class, 'store']);
+
+    // GuideBook endpoints (authenticated)
+    Route::post('/guidebooks', [GuideBookController::class, 'store']);
+    Route::get('/guidebooks/{id}', [GuideBookController::class, 'show']);
+    Route::put('/guidebooks/{id}', [GuideBookController::class, 'update']);
+    Route::delete('/guidebooks/{id}', [GuideBookController::class, 'destroy']);
+
+    // GuideBook Content endpoints (authenticated)
+    Route::get('/guidebooks/{guidebookId}/contents', [GuideContentController::class, 'index']);
+    Route::post('/guidebooks/{guidebookId}/contents', [GuideContentController::class, 'store']);
+    Route::put('/guidebooks/{guidebookId}/contents/{contentId}', [GuideContentController::class, 'update']);
+    Route::delete('/guidebooks/{guidebookId}/contents/{contentId}', [GuideContentController::class, 'destroy']);
 
 });
 
